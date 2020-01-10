d9c64a86f95995e29ef810a8dfa0ddb55588665a
LMnet <lmnet89@gmail.com>
2020-01-10 17:28:48 +0700

    JMX experiments

--- src/main/scala/org/measurer/TestsApp.scala
+++ src/main/scala/org/measurer/TestsApp.scala
@@ -1,9 +1,33 @@
 package org.measurer
 
+import java.lang.management.ManagementFactory
+
 import scala.io.StdIn
+import scala.jdk.CollectionConverters._
+import scala.util.Try
 
 object TestsApp extends App {
+
+  val server = ManagementFactory.getPlatformMBeanServer
+
+  val mbeans = server.queryMBeans(null, null).asScala
+
+  val mbeansMap = mbeans.map { mbean =>
+    val objectName = mbean.getObjectName
+    val beanInfo = server.getMBeanInfo(mbean.getObjectName)
+    val attrs = beanInfo.getAttributes.map { attr =>
+      val attrName = attr.getName
+      attrName -> Try(server.getAttribute(objectName, attr.getName)).toOption
+    }.toMap
+    objectName -> attrs
+  }.toMap
+
+  mbeansMap.foreach { case (k, v) =>
+    println(k)
+    println(v)
+  }
+
   println("Press any key to exit...")
-  StdIn.readLine()
+//  StdIn.readLine()
   println("Exit")
 }

a40e2e1081024eff74bf787179bfe89c937539ac
LMnet <lmnet89@gmail.com>
2020-03-29 11:26:22 +0700

    Experiments

--- build.sbt
+++ build.sbt
@@ -7,5 +7,7 @@ organization := "org.measurer"
 version := "0.0.1-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-  "io.dropwizard.metrics" % "metrics-core" % "4.1.2"
+  "io.dropwizard.metrics" % "metrics-core" % "4.1.2",
+  "io.prometheus" % "simpleclient_common" % "0.8.1",
+  "com.lihaoyi" %% "sourcecode" % "0.2.1",
 )
--- src/main/scala/org/measurer/MetricsCollection.scala
+++ src/main/scala/org/measurer/MetricsCollection.scala
@@ -1,11 +1,34 @@
 package org.measurer
 
-sealed trait Metric
-case class SingleMetric[M](metric: M) extends Metric
-case class LabeledMetric[M](metrics: Map[Label, M]) extends Metric
+import com.codahale.metrics.Counter
 
-case class MetricsCollection(
-  metrics: Map[String, Metric],
+sealed trait NamedMetric {
+  def report: String = toString // TODO: tmp
+}
+object NamedMetric {
+  case class Single[M](name: String, metric: M) extends NamedMetric
+  case class Labeled[M](name: String, labels: Map[String, String], metric: M) extends NamedMetric
+}
+
+class MetricsCollection(
+  prefix: Option[String],
+  metrics: Seq[NamedMetric],
 )
+object MetricsCollection {
+  def apply(metrics: Seq[NamedMetric]) = new MetricsCollection(None, metrics)
+}
 
-case class Label(name: String, value: String)
+case class LabeledMetric()
+
+class MetricsRegistry {
+  protected def named[M](metric: M)(implicit name: sourcecode.Name): M = ???
+  protected def named[M](name: String, metric: M): M = ???
+
+  protected def labeled[M](labels: Map[String, String], metric: M): Map[Map[String, String], M]
+}
+
+
+
+class ModuleMetrics extends MetricsRegistry {
+  val validMessages = named(new Counter())
+}
